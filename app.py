9584729b52d219735f3da52e1ebd11e4d8b0b4f3
Rohith Annameti <91110123+RohithAnnameti@users.noreply.github.com>
2024-10-14 16:39:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-# microservice.py
 from flask import Flask, jsonify, request
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.trace.samplers import ProbabilitySampler
 import logging
 from opencensus.ext.azure.log_exporter import AzureLogHandler
-# Create Flask app
+
 app = Flask(__name__)
+
 # Replace with your actual Instrumentation Key from Azure Application Insights
 INSTRUMENTATION_KEY = 'eb5ed67c-09c1-4217-9d25-c09b90da6343'
+
 # Middleware to automatically trace requests and send telemetry to Azure
 middleware = FlaskMiddleware(
    app,
@@ -18,6 +19,7 @@ middleware = FlaskMiddleware(
 # Configure logging for custom telemetry (optional)
 logger = logging.getLogger(__name__)
 logger.addHandler(AzureLogHandler(connection_string=f'InstrumentationKey={INSTRUMENTATION_KEY}'))
+
 ...
 configure_azure_monitor(
 	enable_live_metrics=True
@@ -26,9 +28,11 @@ configure_azure_monitor(
 
 @app.route("/")
 def hello():
-    return "Welcome to Hello-world Python Application"
+    return "Welcome to Hello-world Python Flask Application"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
+
 
