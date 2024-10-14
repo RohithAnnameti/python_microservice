adf4a6ed3ac186202346d00c27b804a388736046
Rohith Annameti <91110123+RohithAnnameti@users.noreply.github.com>
2024-10-14 12:41:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
 import os
 
 app = Flask(__name__)
+
+# Configure Application Insights
+instrumentation_key = 'eb5ed67c-09c1-4217-9d25-c09b90da6343'  # Replace with your actual Instrumentation Key
+
+# Enable automatic distributed tracing
+middleware = FlaskMiddleware(
+   app,
+   exporter=AzureExporter(connection_string=f'InstrumentationKey={instrumentation_key}'),
+   sampler=ProbabilitySampler(1.0)
+)
 
 @app.route("/")
 def hello():
