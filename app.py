25332c16173ca0c9847f5c1e810838e2a5fa9e16
Rohith Annameti <91110123+RohithAnnameti@users.noreply.github.com>
2024-10-16 12:58:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ import azure.applicationinsights
 app_insights_client = azure.applicationinsights.TelemetryClient(
     instrumentation_key='bba9d30b-eb36-4ab9-a814-434c2e7d86b9'
 )
-app_insights_client.track_event('my_event', {'my_property': 'my_value'})
-app_insights_client.track_metric('my_metric', 100)
-app_insights_client.track_exception(Exception('my_exception'))
-app = Flask(__name__)
 
-azure.applicationinsights.configure(
-    app_insights_client,
-    collection_interval=1.0,
-    collection_type='new'
-)
 @app.route("/")
 def hello():
     return "Welcome to Hello-world Python Flask Application"
