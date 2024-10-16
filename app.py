63b3b3e38b4ede7cb5107538efd453d530fe3dc6
Rohith Annameti <91110123+RohithAnnameti@users.noreply.github.com>
2024-10-16 13:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from flask import Flask
 import os
-import azure.applicationinsights
-
-app_insights_client = azure.applicationinsights.TelemetryClient(
-    instrumentation_key='bba9d30b-eb36-4ab9-a814-434c2e7d86b9'
-)
 
 @app.route("/")
 def hello():
